6a5f2819b3818a9dc728e3d8dcfb4113d2381401
Mac Dowell Prosper <macdowell@noukod.com>
2018-12-17 21:52:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,25 +19,25 @@ class ApplicationController < Sinatra::Base
   post '/posts' do
     Post.create(:name => params[:name], :content => params[:content])
     @posts = Post.all
-    
+
     erb :index
   end
 
   get '/posts' do
     @posts = Post.all
-    
+
     erb :index
   end
 
   get '/posts/:id' do
     @post = Post.find(params[:id])
-    
+
     erb :show
   end
 
   get '/posts/:id/edit' do
     @post = Post.find(params[:id])
-    
+
     erb :edit
   end
 
